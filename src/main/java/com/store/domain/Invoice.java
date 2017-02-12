f1cf7d4e4d5325d263a88a42487950076ec990ac
Andres Piedrahita <andresfph1989@gmail.com>
2017-02-12 18:27:07 +0100

    Creation of Customer and Invoice domains

--- src/main/java/com/store/domain/Invoice.java
+++ src/main/java/com/store/domain/Invoice.java
@@ -1,5 +1,93 @@
 package com.store.domain;
 
+import java.util.Date;
+import java.util.UUID;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
+import org.hibernate.annotations.Type;
+
+@Entity
+@Table(indexes = {
+		@Index(columnList = "invoice_number", name = "invoice_number_index", unique = true),
+		@Index(columnList = "purchase_date", name = "purchase_date_index", unique = false)
+		})
 public class Invoice {
+	
+	@Id
+	@Type(type="org.hibernate.type.PostgresUUIDType")
+	private UUID id;
+	
+	@Column(name="invoice_number")
+	private String invoiceNumber;
+	
+	@Column(name="discount_percentage")
+	private int discountPercentage;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	@Column(name="purchase_date")
+	private Date purchaseDate;
+	
+	@Column(name="total_cost")
+	private double totalCost;
+	
+	@Column(name="tax_percentage")
+	private int taxPercentage;
+
+	public UUID getId() {
+		return id;
+	}
+
+	public void setId(UUID id) {
+		this.id = id;
+	}
+
+	public String getInvoiceNumber() {
+		return invoiceNumber;
+	}
+
+	public void setInvoiceNumber(String invoiceNumber) {
+		this.invoiceNumber = invoiceNumber;
+	}
+
+	public int getDiscountPercentage() {
+		return discountPercentage;
+	}
+
+	public void setDiscountPercentage(int discountPercentage) {
+		this.discountPercentage = discountPercentage;
+	}
+
+	public Date getPurchaseDate() {
+		return purchaseDate;
+	}
+
+	public void setPurchaseDate(Date purchaseDate) {
+		this.purchaseDate = purchaseDate;
+	}
+
+	public double getTotalCost() {
+		return totalCost;
+	}
+
+	public void setTotalCost(double totalCost) {
+		this.totalCost = totalCost;
+	}
+
+	public int getTaxPercentage() {
+		return taxPercentage;
+	}
+
+	public void setTaxPercentage(int taxPercentage) {
+		this.taxPercentage = taxPercentage;
+	}
+	
+	
 
 }
--- src/main/java/com/store/domain/InvoiceRepository.java
+++ src/main/java/com/store/domain/InvoiceRepository.java
@@ -1,5 +1,13 @@
 package com.store.domain;
 
-public interface InvoiceRepository {
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
 
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface InvoiceRepository extends PagingAndSortingRepository<Invoice, UUID>{
+	public Invoice findByInvoiceNumber(@Param("invoiceNumber") String invoiceNumber);
+	public List<Invoice> findByPurchaseDate(@Param("date") Date purchaseDate);
 }
